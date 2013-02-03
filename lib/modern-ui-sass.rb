0f5672993f2ea89f75ca23999612ae3f65d1d6ea
Thorn731 <notdefined@list.ru>
2013-02-03 13:28:48 +0400

    fixed some errors, added compass and rails functions

--- lib/modern-ui-sass.rb
+++ lib/modern-ui-sass.rb
@@ -5,17 +5,37 @@ module ModernUI
   class FrameworkNotFound < StandartError; end
 
   def self.load!
-    if asset_pipeline?
+    if compass? && asset_pipeline?
+      register_compass_extension
+      register_rails_engine
+    elsif compass?
+      # Only require compass extension if a standalone project
+      require 'modern-ui-sass/compass_functions'
+      register_compass_extension
+    elsif asset_pipeline?
       require "sass-rails"
       register_rails_engine
+      require 'modern-ui-sass/rails_functions'
     else
-      raise FrameworkNotFound, "Rails > 3.1 not found"
+      raise ModernUI::FrameworkNotFound, "modern-ui-sass requires either Rails > 3.1 or Compass, neither of which are loaded"
     end
   end
 
   private
+
   def self.asset_pipeline?
     defined?(::Rails) && ::Rails.version >= '3.1.0'
+  end
+
+  def self.compass?
+    defined?(::Compass)
+  end
+
+  def self.register_compass_extension
+    base = File.join(File.dirname(__FILE__), '..')
+    styles = File.join(base, 'vendor', 'assets', 'stylesheets')
+    templates = File.join(base, 'templates')
+    ::Compass::Frameworks.register('modern-ui', :stylesheets_directory => styles, :templates_directory => templates)
   end
 
   def self.register_rails_engine
